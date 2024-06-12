cbabff48fd1ab04d1e2a7f5221f7bddf2bf68004
Peter Park <peterparkk25@gmail.com>
2024-06-11 21:28:15 -0500

    playGame function created simulating 5 rounds. Project complete

--- app.js
+++ app.js
@@ -24,27 +24,56 @@ const playRound = function (humanChoice, computerChoice) {
   if (lowerHumanChoice === "rock") {
     if (computerChoice === "rock")
       console.log("It's a tie! You both chose rock!");
-    else if (computerChoice === "paper")
+    else if (computerChoice === "paper") {
+      computerScore++;
       console.log("You lose! Paper beats rock!");
-    else console.log("You win! Rock beats scissors");
-  } else if (lowerHumanHoice === "paper") {
+    } else {
+      humanScore++;
+      console.log("You win! Rock beats scissors");
+    }
+  } else if (lowerHumanChoice === "paper") {
     if (computerChoice === "paper")
       console.log("It's a tie! You both chose paper!");
-    else if (computerChoice === "scissor")
-      console.log("You win! Rock beats scissors!");
-    else console.log("You lose! Rock beats scissors");
+    else if (computerChoice === "scissor") {
+      computerScore++;
+      console.log("You lose! Scissors beats paper!");
+    } else {
+      humanScore++;
+      console.log("You win! Paper beats rock");
+    }
   } else {
     if (computerChoice === "scissor")
       console.log("It's a tie! You both chose scissors!");
-    else if (computerChoice === "rock")
+    else if (computerChoice === "rock") {
+      computerScore++;
       console.log("You lose! Rock beats scissors!");
-    else console.log("You lose! Rock beats scissors");
+    } else {
+      humanScore++;
+      console.log("You win! Scissors beats paper");
+    }
   }
 };
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
 
-console.log(humanSelection, computerSelection);
+// console.log(humanSelection, computerSelection);
 
-playRound(humanSelection, computerSelection);
+// playRound(humanSelection, computerSelection);
+
+const playGame = function () {
+  for (let i = 0; i < 5; i++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  if (humanScore > computerScore)
+    console.log("You win! You have more points that the computer!");
+  else if (humanScore === computerScore)
+    console.log(
+      "The game is a tie! Both you and the computer have the same score!"
+    );
+  else console.log("You lose, the computer scored more points than you!");
+};
+
+playGame();
