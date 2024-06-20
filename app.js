a6abe28022c873212d0a8f41faee73bc599fd19a
Peter Park <peterparkk25@gmail.com>
2024-06-20 13:23:37 -0500

    buttons and text working, need logic to restart game after # of rounds

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ const getComputerChoice = function () {
 //   return returnValue;
 // };
 
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 
 const playRound = function (humanChoice, computerChoice) {
   const result = document.querySelector(".result");
-  const playerScoreBoard = document.querySelector(".playerCount");
-  const computerScoreBoard = document.querySelector(".computerCount");
+  const scoreBoard = document.querySelector(".scoreBoard");
   let lowerHumanChoice = humanChoice.toLowerCase();
   if (lowerHumanChoice === computerChoice) {
     result.textContent = "It's a tie";
@@ -28,33 +27,28 @@ const playRound = function (humanChoice, computerChoice) {
     if (computerChoice === "paper") {
       computerScore++;
       result.textContent = "You lose! Paper beats rock!";
-      computerScoreBoard.textContent = computerScore;
     } else {
-      humanScore++;
+      playerScore++;
       result.textContent = "You win! Rock beats scissors";
-      playerScoreBoard.textContent = playerScore;
     }
   } else if (lowerHumanChoice === "paper") {
     if (computerChoice === "scissor") {
       computerScore++;
       result.textContent = "You lose! Scissors beats paper!";
-      computerScoreBoard.textContent = computerScore;
     } else {
-      humanScore++;
+      playerScore++;
       result.textContent = "You win! Paper beats rock";
-      playerScoreBoard.textContent = playerScore;
     }
   } else if (lowerHumanChoice === "scissor") {
     if (computerChoice === "rock") {
       computerScore++;
       result.textContent = "You lose! Rock beats scissors!";
-      computerScoreBoard.textContent = computerScore;
     } else {
-      humanScore++;
+      playerScore++;
       result.textContent = "You win! Scissors beats paper";
-      playerScoreBoard.textContent = playerScore;
     }
   }
+  scoreBoard.textContent = `Playerscore: ${playerScore} Computerscore:${computerScore}`;
 };
 
 // playGame();
@@ -65,21 +59,16 @@ const scissorBtn = document.querySelector(".scissor");
 const playerOptions = [rockBtn, scissorBtn, paperBtn];
 
 let rounds = 0;
+let playerChoice;
 
 playerOptions.forEach((option) => {
   option.addEventListener("click", () => {
-    const roundsLeft = document.querySelector(".roundsLeft");
-    rounds++;
-    console.log(this.innerText);
-    roundsLeft.innerText = `Rounds Left: ${5 - rounds}`;
+    // const roundsLeft = document.querySelector(".roundsLeft");
+    // rounds++;
+    playRound(option.innerText, getComputerChoice());
+    // roundsLeft.innerText = `Rounds Left: ${5 - rounds}`;
   });
 });
-
-const container = document.querySelector(".container");
-const result = document.createElement("div");
-result.classList.add("result");
-result.textContent = "Temporary placeholder for result";
-container.appendChild(result);
 
 // //RPS from online
 
